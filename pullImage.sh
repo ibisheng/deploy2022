a523ca49d6a9d534a884c43aecbdfe48a9a22e5b
dan <dan@dandeMBP-3.lan>
2022-05-09 17:47:01 +0800

    修改部署脚本

--- pullImage.sh
+++ pullImage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker pull $imageurl/nginx:1.20.1
-docker pull $imageurl/minio:RELEASE.2021-10-10T16-53-30Z.fipss
+docker pull $imageurl/minio:RELEASE.2021-10-10T16-53-30Z.fips
 docker pull $imageurl/redis:4.0
 docker pull $imageurl/mysql:5.7
 docker pull $imageurl/bsdrive:$tag
