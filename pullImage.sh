4030267687c0cff1912cdacb76d044d8e3885676
dan <dan@dandeMBP-3.lan>
2022-06-26 12:34:45 +0800

    arm 版发版

--- pullImage.sh
+++ pullImage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-docker pull $imageurl/nginx:1.20.1
-docker pull $imageurl/minio:RELEASE.2021-10-10T16-53-30Z.fips
-docker pull $imageurl/redis:4.0
-docker pull $imageurl/mysql:5.7
+docker pull $imageurl/nginx_arm:1.20.1
+docker pull $imageurl/minio_arm:RELEASE.2021-10-10T16-53-30Z.fips
+docker pull $imageurl/rejson_arm:2.0.6
+docker pull $imageurl/mysql_arm:5.7
 docker pull $imageurl/bsdrive_arm:$tag
 docker pull $imageurl/bso_arm:$tag
