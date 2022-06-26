7022ceb181dc2306f9e095bb2cc0f957be2abf5b
dan <dan@dandeMBP-3.lan>
2022-06-26 14:28:39 +0800

    arm 版发版

--- pullImage.sh
+++ pullImage.sh
@@ -2,6 +2,6 @@
 docker pull $imageurl/nginx_arm:1.20.1
 docker pull $imageurl/minio_arm:RELEASE.2021-10-10T16-53-30Z
 docker pull $imageurl/rejson_arm:2.0.6
-docker pull $imageurl/mysql_arm:5.7
+docker pull $imageurl/mysql_arm:8-oracle
 docker pull $imageurl/bsdrive_arm:$tag
 docker pull $imageurl/bso_arm:$tag
