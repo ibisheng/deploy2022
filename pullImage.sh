574decb0244b30122cbfc25d8bb9e6b252d1549b
dan <dan@dandeMBP-3.lan>
2022-06-30 14:30:01 +0800

    镜像提交

--- pullImage.sh
+++ pullImage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker pull $imageurl/nginx:1.20.1
 docker pull $imageurl/minio:RELEASE.2021-10-10T16-53-30Z.fips
-docker pull $imageurl/redis:4.0
-docker pull $imageurl/mysql:5.7
+docker pull $imageurl/rejson:2.0.9
+docker pull $imageurl/mysql:8-oracle
 docker pull $imageurl/bsdrive:$tag
 docker pull $imageurl/bso:$tag
