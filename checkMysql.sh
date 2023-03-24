ae2c0e0b4892ccb81331da4dd01a8c925360c76b
dan <dan@dandeMBP-3.lan>
2023-03-24 18:42:42 +0800

    移除安装时的重启

--- checkMysql.sh
+++ checkMysql.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 mysql_status=1
+
+MYSQL_USER=root
+MYSQL_PASSWORD=123456
 until [ $mysql_status -eq 0 ]
 do
   docker exec -i mysql sh -c 'exec mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "select now()"'
